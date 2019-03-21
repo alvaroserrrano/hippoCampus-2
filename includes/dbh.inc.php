ff71dc52ea71394f02b5245f10ae9bbe7eeb4e11
Rivas <43758471+alvaroserrrano@users.noreply.github.com>
2019-03-21 12:59:41 -0400

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,1 +1,13 @@
+$serverName = 'localhost';
+$userName = 'alvaro';
+$password = 'Aserrano1';
+$dbName = "loginsystem";
 
+
+
+$connect = mysqli_connect($serverName, $userName, $password, $dbname);
+
+// Check connection
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
