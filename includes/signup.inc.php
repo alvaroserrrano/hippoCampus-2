663cb1eb34a1a4ee14570052e1e80f3353ee38f1
Alvaro <alvaro.serrano@wmich.edu>
2019-03-21 15:36:50 -0400

    worked on signup

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,1 +1,21 @@
+<?php
 
+if(isset($_POST['signup-submit'])){
+    
+    require 'dhb.inc.php';
+
+    $username = test_input($_POST['uid']);
+    $email = test_input($_POST['mailuid']);
+    $password = test_input($_POST['pwd']);
+    $passwordRepeat = test_input($_POST['pwdRepeat']);
+
+
+}
+
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
