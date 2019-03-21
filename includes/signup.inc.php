5ac2c9273496ee7393069aa9fb45e0652ba6fb33
Alvaro <alvaro.serrano@wmich.edu>
2019-03-21 17:33:18 -0400

    advance signup

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -4,18 +4,33 @@ if(isset($_POST['signup-submit'])){
     
     require 'dhb.inc.php';
 
-    $username = test_input($_POST['uid']);
-    $email = test_input($_POST['mailuid']);
-    $password = test_input($_POST['pwd']);
-    $passwordRepeat = test_input($_POST['pwdRepeat']);
+    $userName = mysqli_real_scape_string($connect, $_POST['uid']);
+    $email = mysqli_real_scape_string($connect, $_POST['mailuid']);
+    $password = mysqli_real_scape_string($connect, $_POST['pwd']);
+    $passwordRepeat = mysqli_real_scape_string($connect, $_POST['pwdRepeat']);
 
+    //Check if any field is empty
+    if (empty($userName) || empty($last) || empty($email) || empty($uid) || empty($pwd) ) {
+        header("Location: ../signup.php?signup=empty");
+    exit();
+    }else{
+        //Check if userName is valid
+        if(!preg_match("/^[a-zA-Z]*$/", $userName)){
+            header("Location: ../signup.php?signup=invalid");
+            exit();    
+        }else{
+            //Check email is valid
+            if(!filter_var("$email", FILTER_VALIDATE_EMAIL)){
+                header("Location: ../signup.php?signup=email");
+            exit();
+            }
+            else{
+                $sql = "SELECT * FROM users WHERE username = '$userName";
+                $result = mysqli_query($connect, $sql);
+            }
+        }
+    }
 
-}
-
-
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+}else{
+    header("Location: ../signup.php");
 }
