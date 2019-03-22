5123ea6de60827a0cd1086f0c70f8b01b5bbd33a
Rivas <43758471+alvaroserrrano@users.noreply.github.com>
2019-03-22 10:05:36 -0400

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,1 +1,18 @@
+<?php
 
+if(isset($_POST['login-submit'])){
+
+    require 'dbh.inc.php';
+
+    $email = $_POST['mailuid'];
+    $password = $_POST['pwd'];
+
+    if(empty($email) || empty($password))){
+        header("Location: ..index.php?error=emptyfields");
+    exit();
+    }
+
+}else{
+    header("Location: ..index.php");
+    exit();
+}
