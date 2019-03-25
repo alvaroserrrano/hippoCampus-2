c75cc47b39a8ab1ae4b6345b99e8a7dc86750626
Alvaro <alvaro.serrano@wmich.edu>
2019-03-25 19:34:26 -0400

    created contact form

--- contact.php
+++ contact.php
@@ -1,1 +1,22 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+    <main>
+        <p>Your opinion matters!</p>
+        <form class="contactForm" action="contactform.php" method="post">
+            <input type="text" name="name" id="" placeholder="Full name">
+            <input type="text" name="mail" id="" placeholder="Email">
+            <input type="text" name="subject" id="" placeholder="Subject">
+            <input type="text" name="message" id="" placeholder="Message">
+            <button type="submit">SEND MAIL</button>
+        </form>
+    </main>
+</body>
+</html>
