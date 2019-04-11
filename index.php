30ffd6307eab546e91a497825792eb514b8cc477
Alvaro <alvaro.serrano@wmich.edu>
2019-04-11 15:12:02 -0400

    added navbar

--- index.php
+++ index.php
@@ -11,6 +11,10 @@ if(!isset ($_SESSION['user_id'])){
         <title>Hippo Campus Live Chat Application</title>  
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"   crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.rawgit.com/mervick/emojionearea/master/dist/emojionearea.min.css">
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -55,9 +59,35 @@ if(!isset ($_SESSION['user_id'])){
             cursor: pointer;
         }
 
+
     </style>  
 
     <body>  
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="bambi2.html">Bambi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="landmark2.html">Landmark</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="budget.html">Finance</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">Developers</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
         <div class="container">
             <br />
             
@@ -86,23 +116,6 @@ if(!isset ($_SESSION['user_id'])){
         <input type="hidden" id="is_active_group_chat_window" value="no" />
         <button style="margin-left:150px; margin-bottom: 50px; margin-top: 50px type="button" name="group_chat" id="group_chat" class="btn btn-warning btn-xs">Group Chat</button>
             <br>
-        <div class="buttons" style="height:20%">
-                
-                <button type="button" class="btn btn-primary btn-xs"><a style="margin: 20px;text-decoration: none; color: #fff" href="main.php">Main page</a></button>
-                </liclas>
-                
-                <button type="button" class="btn btn-primary btn-xs"><a style="margin-top: 20px;text-decoration: none; color: #fff" href="bambi2.html">Bambi</a></button>
-                
-                
-                <button type="button" class="btn btn-primary btn-xs"><a style="margin-top: 20px;text-decoration: none; color: #fff" href="landmark2.html">Landmark</a></button>
-                
-                
-                <button type="button" class="btn btn-primary btn-xs"><a style="margin-top: 20px;text-decoration: none; color: #fff" href="budget.html">Finance</a></button>
-                
-                
-                <button type="button" class="btn btn-primary btn-xs"><a style="margin-top: 20px;text-decoration: none; color: #fff" href="about.php">Developers</a></button>
-                
-        </div>
 
     </body>  
 </html>  
